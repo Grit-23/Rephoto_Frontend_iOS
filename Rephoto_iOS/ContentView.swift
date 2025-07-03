cd98564bc66897fc135d5d3dee0bf4599c5578a8
Doyeon Kim <ehdus022191@dgu.ac.kr>
2025-07-03 15:21:42 +0900

    Add Xcode project

---
+++ Rephoto_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rephoto_iOS
+//
+//  Created by 김도연 on 7/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rephoto_iOS/Rephoto_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rephoto_iOSApp.swift
+//  Rephoto_iOS
+//
+//  Created by 김도연 on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Rephoto_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
