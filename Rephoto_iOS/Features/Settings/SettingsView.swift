00fb1d1719bf5e659a615a8bdac9be652692f108
Doyeon Kim <ehdus022191@dgu.ac.kr>
2025-07-08 19:16:01 +0900

    [Chore] SettingsViewFile

---
+++ Rephoto_iOS/Features/Settings/SettingsView.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsView.swift
+//  Rephoto_iOS
+//
+//  Created by 김도연 on 7/8/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("SettingsView")
+    }
+}
