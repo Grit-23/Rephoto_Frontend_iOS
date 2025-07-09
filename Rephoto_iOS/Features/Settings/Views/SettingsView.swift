59f2775a083550d572032d557798ec9a128873f8
Doyeon Kim <ehdus022191@dgu.ac.kr>
2025-07-10 00:22:21 +0900

    [Chore] SettingComponentFiles

---
+++ Rephoto_iOS/Features/Settings/Views/SettingsView.swift
@@ -0,0 +1,40 @@
+//
+//  SettingsView.swift
+//  Rephoto_iOS
+//
+//  Created by 김도연 on 7/8/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    @Binding var sheetDetent: PresentationDetent
+    @Environment(\.dismiss) private var dismiss
+
+    var body: some View {
+        VStack {
+            Text("Settings")
+                .font(.largeTitle)
+            Spacer()
+        }
+        .padding()
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button {
+                    sheetDetent = .medium
+                    dismiss()
+                } label: {
+                    HStack(spacing: 4) {
+                        Image(systemName: "chevron.left")
+                    }
+                }
+            }
+        }
+        .tint(.black)
+    }
+}
+
+#Preview {
+    HomeSheetView(sheetDetent: .constant(.medium))
+}
