0d0059c6175ee5a29796f973c26f6d0dcdb32d38
Doyeon Kim <ehdus022191@dgu.ac.kr>
2026-05-17 22:25:25 +0900

    fix: RephotoTabView에서 HomeView에 provider 주입 누락 수정

--- Rephoto_iOS/Features/RephotoTabView.swift
+++ Rephoto_iOS/Features/RephotoTabView.swift
@@ -7,12 +7,15 @@
 
 import SwiftUI
 import PhotosUI
+import Factory
 
 struct RephotoTabView: View {
+    @Injected(\.homeUseCaseProvider) private var homeProvider
+
     var body : some View {
         TabView{
             Tab("홈", image: "home") {
-                HomeView()
+                HomeView(provider: homeProvider)
             }
             Tab("Search", systemImage: "magnifyingglass", role: .search) {
                 SearchView()
